245e1f11fecb5edd11fd07ffeab3738067275b4a
tjgus <tjgus@DESKTOP-6N2N8JC>
2021-09-29 22:03:19 +0900

    2.0a

--- ArcaliveCrawler/MainForm.Designer.cs
+++ ArcaliveCrawler/MainForm.Designer.cs
@@ -29,20 +29,11 @@ namespace ArcaliveCrawler
         /// </summary>
         private void InitializeComponent()
         {
-            this.versionCheckResultLabel = new System.Windows.Forms.Label();
             this.CrawlButton = new System.Windows.Forms.Button();
             this.StatExportButton = new System.Windows.Forms.Button();
             this.versionLabel = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // versionCheckResultLabel
-            // 
-            this.versionCheckResultLabel.AutoSize = true;
-            this.versionCheckResultLabel.Location = new System.Drawing.Point(12, 249);
-            this.versionCheckResultLabel.Name = "versionCheckResultLabel";
-            this.versionCheckResultLabel.Size = new System.Drawing.Size(119, 12);
-            this.versionCheckResultLabel.TabIndex = 1;
-            this.versionCheckResultLabel.Text = "VersionCheckResult";
             // 
             // CrawlButton
             // 
@@ -74,15 +65,25 @@ namespace ArcaliveCrawler
             this.versionLabel.Text = "VERSION";
             this.versionLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(10, 249);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 12);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(272, 270);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.versionLabel);
             this.Controls.Add(this.StatExportButton);
             this.Controls.Add(this.CrawlButton);
-            this.Controls.Add(this.versionCheckResultLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
             this.Text = "아카크롤러 2";
@@ -93,10 +94,10 @@ namespace ArcaliveCrawler
         }
 
         #endregion
-        private System.Windows.Forms.Label versionCheckResultLabel;
         private System.Windows.Forms.Button CrawlButton;
         private System.Windows.Forms.Button StatExportButton;
         private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- ArcaliveCrawler/MainForm.cs
+++ ArcaliveCrawler/MainForm.cs
@@ -4,10 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ArcaliveForm;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace ArcaliveCrawler
 {
@@ -23,12 +25,30 @@ namespace ArcaliveCrawler
         {
             versionLabel.Text = currentVersion;
             #region 버전 체크
-            string checkedVersion = string.Empty;
 
-            if (currentVersion == checkedVersion)
-                versionCheckResultLabel.Text = "최신 버전입니다";
-            else
-                versionCheckResultLabel.Text = "업데이트 가능";
+            try
+            {
+                string tmp = string.Empty;
+                using (WebClient wc = new WebClient())
+                {
+                    tmp = wc.DownloadString("https://github.com/csh1668/ArcaliveCrawler/releases");
+                }
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(tmp);
+                var releases = doc.DocumentNode.SelectNodes("//div[contains(@class, 'release-entry')]");
+                var latestRelease = releases[0].SelectSingleNode("//div[contains(@class, 'd-flex flex-items-start')]/div[1]/a").InnerText;
+
+                if (currentVersion == latestRelease)
+                    linkLabel1.Text = "최신 버전입니다";
+                else
+                    linkLabel1.Text = "업데이트 가능";
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                linkLabel1.Text = "GitHub 연결 실패";
+            }
 
             #endregion
         }
